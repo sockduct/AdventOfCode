77c4c5cb88f7b576d48d0bb633f4ed1d5274e243
James Small <james.r.small@outlook.com>
2021-12-17 22:32:46 -0500

    Update:  Continue working on day 6, part 2 challenge

--- 2021/d6p2.py
+++ 2021/d6p2.py
@@ -3,42 +3,21 @@
 INFILE = 'd6p1t1.txt'
 # INFILE = 'd6p1.txt'
 
-class LatternFish():
-    DEFAULT_NEW_AGE = 8
-    STARTING_AGE = 6
-
-    def __init__(self, age=DEFAULT_NEW_AGE):
-        self.age = age
-
-    def __repr__(self):
-        return f'<LatternFish(age={self.age})>'
-
-    def __str__(self):
-        return f'{self.age}'
-
-    def inc(self):
-        'Age fish a day, reset and procreate after age < 0'
-        self.age -= 1
-        if self.age == -1:
-            self.age = LatternFish.STARTING_AGE
-            return True
-        return False
-
+'''
+Lots of thought and effort to improve upon d6p1 approach and this is actually
+slower!!!
+'''
 def main(verbose=False):
     with open(INFILE) as ifile:
         fish_ages = ''.join(ifile.read().strip().split(','))
 
-    # fish = [LatternFish(fish_age) for fish_age in fish_ages]
-
-    DAYS = 18
+    # DAYS = 18
     # DAYS = 80
-    # Doesn't work for large numbers because this is a quadratic algorithm:
-    # DAYS = 256
+    DAYS = 256
     new_fish = 0
     for day in range(DAYS + 1):
         if day == 0:
-            # print(f'Initial state:  {", ".join(map(str, fish))}')
-            print(f'Initial state:  {fish_ages}')
+            print(f'Initial state:  {", ".join(fish_ages)}')
         else:
             '''
             new_fish = sum(lf.inc() for lf in fish)
@@ -54,19 +33,28 @@ def main(verbose=False):
             else:
                 print('.', end='', flush=True)
             '''
-            if new_fish:
-                fish_ages += '9' * new_fish
-            diff_num = '1' * len(fish_ages)
-            fish_ages = str(int(fish_ages) - int(diff_num))
+            new_fish = 0
             zeroes = fish_ages.count('0')
             if zeroes:
                 fish_ages = fish_ages.replace('0', '7')
                 new_fish = zeroes
+            if new_fish:
+                fish_ages += '9' * new_fish
+            diff_num = '1' * len(fish_ages)
+            fish_ages = str(int(fish_ages) - int(diff_num))
+            if len(fish_ages) < len(diff_num):
+                fish_ages = '0' * (len(diff_num) - len(fish_ages)) + fish_ages
 
-            daystr = 'days' if day > 1 else 'day'
-            print(f'After {day:2} {daystr:4}:  {fish_ages}')
+            if verbose:
+                daystr = 'days' if day > 1 else 'day'
+                print(f'After {day:2} {daystr:4}:  {fish_ages}')
+            elif day == 1:
+                print('Days:  ', end='')
+            elif day % 10 == 0:
+                print(f'{day}', end='', flush=True)
+            else:
+                print('.', end='', flush=True)
 
-    # print(f'Lattern Fish Total:  {len(fish)}')
     print(f'Lattern Fish Total:  {len(fish_ages)}')
 
 if __name__ == '__main__':
