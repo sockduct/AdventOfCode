c125d06d19b25f6767aef8a69937bfc2e5ddbb1a
James Small <james.r.small@outlook.com>
2022-01-02 19:09:01 -0500

    Update:  Continue working on day 9, part 2

--- 2021/d9p2.py
+++ 2021/d9p2.py
@@ -44,31 +44,52 @@ def find_basin_points(ri, ci, rows, cols, row, matrix):
     All points surrounding a low point (including the low point).  The edge of
     a basin is an item with a height (value) of 9.
 
-    Recursively check left, above, right, and below.
+    Check left, above, right, and below.
     '''
     HIGH_POINT = 9
 
-    '''
-    # Fail!
-    * Can't go left and then right because infinite recursion results
-    * Perhaps go only left and up, then...?
-
-    Need to think through approach...
-    '''
+    cur_left = ci
+    cur_right = ci
+    total_points = 1  # Start with including low point (ri, ci)
     # Check left:
-    if ci > 0 and row[ci - 1] < HIGH_POINT:
-        return 1 + find_basin_points(ri, ci - 1, rows, cols, row, matrix)
+    while cur_left > 0 and row[cur_left - 1] < HIGH_POINT:
+        cur_left -= 1
     # Check right:
-    if ci + 1 < cols and row[ci + 1] < HIGH_POINT:
-        return 1 + find_basin_points(ri, ci + 1, rows, cols, row, matrix)
-    # Check above:
-    if ri > 0 and matrix[ri - 1][ci] < HIGH_POINT:
-        return 1 + find_basin_points(ri - 1, ci, rows, cols, matrix[ri - 1], matrix)
-    # Check below:
-    if ri + 1 < rows and matrix[ri + 1][ci] < HIGH_POINT:
-        return 1 + find_basin_points(ri + 1, ci, rows, cols, matrix[ri + 1], matrix)
-    # Surrounded by high points (base case)
-    return 1
+    while cur_right + 1 < cols and row[cur_right + 1] < HIGH_POINT:
+        cur_right += 1
+
+    total_points += cur_right - cur_left
+
+    def find_row_points_up(row, ref_inbasin, cur_row):
+        inbasin = [(val and row[i] < HIGH_POINT) for i, val in enumerate(ref_inbasin)]
+        if cur_row > 0 and any(inbasin):
+            return sum(inbasin) + find_row_points_up(matrix[cur_row - 1], inbasin, cur_row - 1)
+
+        return sum(inbasin)
+
+    # Recursively check above:
+    # Make sure that row above connected to valid point in row below - might not
+    # be contiguous:
+    if ri > 0:
+        inbasin = [(cur_left <= i <= cur_right) for i in range(len(row))]
+        total_points += find_row_points_up(matrix[ri - 1], inbasin, ri - 1)
+
+    def find_row_points_down(row, ref_inbasin, cur_row, rows):
+        inbasin = [(val and row[i] < HIGH_POINT) for i, val in enumerate(ref_inbasin)]
+        if cur_row + 1 < rows and any(inbasin):
+            return sum(inbasin) + find_row_points_down(matrix[cur_row + 1], inbasin,
+                                                       cur_row + 1, rows)
+
+        return sum(inbasin)
+
+    # Recurively check below:
+    # Make sure that row below connected to valid point in row above - might not
+    # be contiguous:
+    if ri + 1 < rows:
+        inbasin = [(cur_left <= i <= cur_right) for i in range(len(row))]
+        total_points += find_row_points_down(matrix[ri + 1], inbasin, ri + 1, rows)
+
+    return total_points
 
 
 def main():
@@ -79,13 +100,19 @@ def main():
 
     rows = len(matrix)
     cols = len(matrix[0])
+    lowpoints = []
     for ri, row in enumerate(matrix):
         for ci, item in enumerate(row):
             if is_lowpoint(ri, ci, rows, cols, item, row, matrix):
                 print(f'Found low point at ({ri}, {ci}):  {item}')
-                basin_size = find_basin_points(ri, ci, rows, cols, row, matrix)
-                print(f'Basin size:  {basin_size}')
+                lowpoints.append((ri, ci))
 
+    '''
+    Close - off by 1 on 3rd basin size...
+    '''
+    for ri, ci in lowpoints:
+        basin_size = find_basin_points(ri, ci, rows, cols, matrix[ri], matrix)
+        print(f'Basin size:  {basin_size}')
 
 if __name__ == '__main__':
     main()
