a1c8fad6d8fd8cd301a9b1057a294f2be874829a
James Small <james.r.small@outlook.com>
2022-01-07 22:41:12 -0500

    Update:  Continue working on day 9, part 2
    Try different approach in find_basin_points and some refactoring
    Update .gitignore to ignore undo files

--- 2021/d9p2.py
+++ 2021/d9p2.py
@@ -17,11 +17,16 @@ Find the low points - locations lower than any adjacent location.  Adjacencies
 include items to the left, right, up, or down, but not diagonally.
 '''
 
-def is_lowpoint(ri, ci, rows, cols, item, row, matrix):
+def is_lowpoint(ri, ci, matrix):
     '''
     A low point is an item that is less than items to the left, right, above
     and below.
     '''
+    rows = len(matrix)
+    cols = len(matrix[0])
+    item = matrix[ri][ci]
+    row = matrix[ri]
+
     # Check for failure cases
     # Check left:
     if ci > 0 and row[ci - 1] <= item:
@@ -39,7 +44,7 @@ def is_lowpoint(ri, ci, rows, cols, item, row, matrix):
     return True
 
 
-def find_basin_points(ri, ci, rows, cols, row, matrix):
+def find_basin_points(ri, ci, matrix):
     '''
     Assumes point passed is a low point.
 
@@ -49,79 +54,170 @@ def find_basin_points(ri, ci, rows, cols, row, matrix):
     Check left, above, right, and below.
     '''
     HIGH_POINT = 9
+    rows = len(matrix)
+    cols = len(matrix[0])
+    item = matrix[ri][ci]
+    row = matrix[ri]
+    basin_matrix = [ [False] * cols ] * rows
 
-    cur_left = ci
-    cur_right = ci
-    total_points = 1  # Start with including low point (ri, ci)
+    ledge = ci
+    redge = ci
+    # Track contiguous low points through shadow matrix:
+    basin_matrix[ri][ci] = True
+
     # Check left:
-    while cur_left > 0 and row[cur_left - 1] < HIGH_POINT:
-        cur_left -= 1
+    while ledge > 0 and row[ledge - 1] < HIGH_POINT:
+        ledge -= 1
+        basin_matrix[ri][ledge] = True
     # Check right:
-    while cur_right + 1 < cols and row[cur_right + 1] < HIGH_POINT:
-        cur_right += 1
+    while redge + 1 < cols and row[redge + 1] < HIGH_POINT:
+        redge += 1
+        basin_matrix[ri][redge] = True
 
-    total_points += cur_right - cur_left
+    def find_row_points_up(basin_matrix, cur_row):
+        cur_left = len(row) + 1
+        cur_right = -1
 
-    def find_row_points_up(row, ref_inbasin, cur_row):
-        # Find low points in row above low points in row below:
-        inbasin = [(val and row[i] < HIGH_POINT) for i, val in enumerate(ref_inbasin)]
+        for col, val in enumerate(basin_matrix[cur_row + 1]):
+            basin_matrix[cur_row][col] = val and matrix[cur_row][col] < HIGH_POINT
+            # Find outermost edges:
+            if basin_matrix[cur_row][col] and col < cur_left:
+                cur_left = col
+            if basin_matrix[cur_row][col] and col > cur_right:
+                cur_right = col
 
         # Check for low points to left and right in current row:
-        for i, val in enumerate(inbasin):
-            if val and i > 0 and not inbasin[i - 1]:
-                temp_i = i - 1
-                while temp_i >= 0 and row[temp_i] < HIGH_POINT:
-                    inbasin[temp_i] = True
-                    temp_i -= 1
-            if val and i + 1 < cols and not inbasin[i + 1]:
-                temp_i = i + 1
-                while temp_i < cols and row[temp_i] < HIGH_POINT:
-                    inbasin[temp_i] = True
-                    temp_i += 1
+        for col, val in enumerate(basin_matrix[cur_row]):
+            '''
+            # Believe redundant from above:
+            if val and col < cur_left:
+                cur_left = col
+            if val and col > cur_right:
+                cur_right = col
+            '''
 
-        if cur_row > 0 and any(inbasin):
-            return sum(inbasin) + find_row_points_up(matrix[cur_row - 1], inbasin, cur_row - 1)
+            if val and col > 0 and not basin_matrix[cur_row][col - 1]:
+                cur_col = col - 1
+                while cur_col >= 0 and basin_matrix[cur_row][cur_col] < HIGH_POINT:
+                    basin_matrix[cur_row][cur_col] = True
+                    cur_left = cur_col
+                    cur_col -= 1
+            if val and col + 1 < cols and not basin_matrix[cur_row][col + 1]:
+                cur_col = col + 1
+                while cur_col < cols and basin_matrix[cur_row][cur_col] < HIGH_POINT:
+                    basin_matrix[cur_row][cur_col] = True
+                    cur_right = cur_col
+                    cur_col += 1
 
-        return sum(inbasin)
+        '''
+        When go up and down:
+        * Keep track of previous row left and right edges
+        * If current row is wider left or right edge, then check above/below wider
+          points down to starting/home row
+          * From above, down to home row
+          * from below, up to home row + 1
+        '''
+        if cur_left < ledge:
+            cur_col = cur_left
+            while cur_col < ledge:
+                temp_row = cur_row
+                while temp_row <= ri:
+                    if matrix[temp_row][cur_col] < HIGH_POINT:
+                        basin_matrix[temp_row][cur_col] = True
+                    temp_row += 1
+                cur_col += 1
+        if cur_right > redge:
+            cur_col = cur_right
+            while cur_col > redge:
+                temp_row = cur_row
+                while temp_row <= ri:
+                    if matrix[temp_row][cur_col] < HIGH_POINT:
+                        basin_matrix[temp_row][cur_col] = True
+                    temp_row += 1
+                cur_col -= 1
+
+        if cur_row > 0 and any(basin_matrix[cur_row]):
+            find_row_points_up(basin_matrix, cur_row - 1)
 
     # Recursively check above:
     # Make sure that row above connected to valid point in row below - might not
     # be contiguous:
     if ri > 0:
-        inbasin = [(cur_left <= i <= cur_right) for i in range(len(row))]
-        total_points += find_row_points_up(matrix[ri - 1], inbasin, ri - 1)
+        find_row_points_up(basin_matrix, ri - 1)
 
-    def find_row_points_down(row, ref_inbasin, cur_row, rows):
-        inbasin = [(val and row[i] < HIGH_POINT) for i, val in enumerate(ref_inbasin)]
+    def find_row_points_down(basin_matrix, cur_row):
+        cur_left = len(row) + 1
+        cur_right = -1
+
+        for col, val in enumerate(basin_matrix[cur_row - 1]):
+            basin_matrix[cur_row][col] = val and matrix[cur_row][col] < HIGH_POINT
+            # Find outermost edges:
+            if basin_matrix[cur_row][col] and col < cur_left:
+                cur_left = col
+            if basin_matrix[cur_row][col] and col > cur_right:
+                cur_right = col
 
         # Check for low points to left and right in current row:
-        for i, val in enumerate(inbasin):
-            if val and i > 0 and not inbasin[i - 1]:
-                temp_i = i - 1
-                while temp_i >= 0 and row[temp_i] < HIGH_POINT:
-                    inbasin[temp_i] = True
-                    temp_i -= 1
-            if val and i + 1 < cols and not inbasin[i + 1]:
-                temp_i = i + 1
-                while temp_i < cols and row[temp_i] < HIGH_POINT:
-                    inbasin[temp_i] = True
-                    temp_i += 1
+        for col, val in enumerate(basin_matrix[cur_row]):
+            '''
+            # Believe redundant from above:
+            if val and col < cur_left:
+                cur_left = col
+            if val and col > cur_right:
+                cur_right = col
+            '''
 
-        if cur_row + 1 < rows and any(inbasin):
-            return sum(inbasin) + find_row_points_down(matrix[cur_row + 1], inbasin,
-                                                       cur_row + 1, rows)
+            if val and col > 0 and not basin_matrix[cur_row][col - 1]:
+                cur_col = col - 1
+                while cur_col >= 0 and basin_matrix[cur_row][cur_col] < HIGH_POINT:
+                    basin_matrix[cur_row][cur_col] = True
+                    cur_left = cur_col
+                    cur_col -= 1
+            if val and col + 1 < cols and not basin_matrix[cur_row][col + 1]:
+                cur_col = col + 1
+                while cur_col < cols and basin_matrix[cur_row][cur_col] < HIGH_POINT:
+                    basin_matrix[cur_row][cur_col] = True
+                    cur_right = cur_col
+                    cur_col += 1
 
-        return sum(inbasin)
+        '''
+        When go up and down:
+        * Keep track of previous row left and right edges
+        * If current row is wider left or right edge, then check above/below wider
+          points down to starting/home row
+          * From above, down to home row
+          * from below, up to home row + 1
+        '''
+        if cur_left < ledge:
+            cur_col = cur_left
+            while cur_col < ledge:
+                temp_row = cur_row
+                while temp_row > ri:
+                    if matrix[temp_row][cur_col] < HIGH_POINT:
+                        basin_matrix[temp_row][cur_col] = True
+                    temp_row -= 1
+                cur_col += 1
+        if cur_right > redge:
+            cur_col = cur_right
+            while cur_col > redge:
+                temp_row = cur_row
+                while temp_row > ri:
+                    if matrix[temp_row][cur_col] < HIGH_POINT:
+                        basin_matrix[temp_row][cur_col] = True
+                    temp_row -= 1
+                cur_col -= 1
+
+        if cur_row + 1 < rows and any(basin_matrix[cur_row]):
+            find_row_points_down(basin_matrix, cur_row + 1)
 
     # Recurively check below:
     # Make sure that row below connected to valid point in row above - might not
     # be contiguous:
     if ri + 1 < rows:
         # Find low points in row above low points in row below:
-        inbasin = [(cur_left <= i <= cur_right) for i in range(len(row))]
-        total_points += find_row_points_down(matrix[ri + 1], inbasin, ri + 1, rows)
+        find_row_points_down(basin_matrix, ri + 1)
 
-    return total_points
+    return sum(sum(row) for row in basin_matrix)
 
 
 def main(verbose=False):
@@ -130,19 +226,17 @@ def main(verbose=False):
         for line in infile:
             matrix.append([int(i) for i in line.strip()])
 
-    rows = len(matrix)
-    cols = len(matrix[0])
     lowpoints = []
     for ri, row in enumerate(matrix):
         for ci, item in enumerate(row):
-            if is_lowpoint(ri, ci, rows, cols, item, row, matrix):
+            if is_lowpoint(ri, ci, matrix):
                 if verbose:
                     print(f'Found low point at ({ri}, {ci}):  {item}')
                 lowpoints.append((ri, ci))
 
     basinsizes = []
     for ri, ci in lowpoints:
-        basin_size = find_basin_points(ri, ci, rows, cols, matrix[ri], matrix)
+        basin_size = find_basin_points(ri, ci, matrix)
         if verbose:
             print(f'Basin size:  {basin_size}')
         basinsizes.append(basin_size)
@@ -157,6 +251,8 @@ def main(verbose=False):
     * Keep track of previous row left and right edges
     * If current row is wider left or right edge, then check above/below wider
       points down to starting/home row
+      * From above, down to home row
+      * from below, up to home row + 1
     '''
     # Multiply three largest basins:
     res = reduce(lambda x, y: x * y, sorted(basinsizes, reverse=True)[:3])
