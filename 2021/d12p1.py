115bcb2b323ba3584eb7254fb9cdb2ab7192f577
James Small <james.r.small@outlook.com>
2022-01-15 17:18:11 -0500

    Add:  Begin working on day 12, part 1 code challenge

---
+++ 2021/d12p1.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python3.10
+
+INFILE = 'd12p1t1.txt'
+# INFILE = 'd12p1t2.txt'
+# INFILE = 'd12p1t3.txt'
+# INFILE = 'd12p1.txt'
+
+class Graph:
+    ...
+
+
+def main():
+    with open(INFILE) as infile:
+        ...
+
+
+if __name__ == '__main__':
+    main()
