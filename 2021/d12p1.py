7284c98be67366e2233f168a29cd26ec144f0d4e
James Small <james.r.small@outlook.com>
2022-01-16 17:56:30 -0500

    Add/Update:  Leverage graph ADT from DSAP, incorporate into part 1

--- 2021/d12p1.py
+++ 2021/d12p1.py
@@ -1,17 +1,26 @@
 #! /usr/bin/env python3.10
 
+from pprint import pprint
+
+from graph import Graph
+
 INFILE = 'd12p1t1.txt'
 # INFILE = 'd12p1t2.txt'
 # INFILE = 'd12p1t3.txt'
 # INFILE = 'd12p1.txt'
 
-class Graph:
-    ...
-
 
 def main():
+    cave_graph = Graph()
     with open(INFILE) as infile:
-        ...
+        for edge in infile:
+            start_vertex, end_vertex = edge.split('-')
+            # Check if already present?
+            sv = cave_graph.insert_vertex(start_vertex)
+            ev = cave_graph.insert_vertex(end_vertex)
+            cave_graph.insert_edge(sv, ev)
+
+    pprint(cave_graph.__dict__)
 
 
 if __name__ == '__main__':
