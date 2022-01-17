e3da3d66d56b321b68b7849af0fbf9cbb634b364
James Small <james.r.small@outlook.com>
2022-01-17 16:51:58 -0500

    Add/Update:  Continue working on day 12, part 1 plus
    graph.py:
    * Extensive refactoring:
      * Moved embedded classes to the end
      * Renamed elements/x to labels
      * Add label dictionary to map labels to vertices
      * Require labels for vertices, optional for edges
      * Manage labels with properties
      * Add __repr__ and __str__ for all 3 classes
      * Fixed _validate_vertex so it works as intended
      * Add format_output method for nice display of vertices and edges of a
        Graph
      * Refactor incident_edges to use yield from versus older for iterable:
        yield ...
      * Refactor insert_edge to add edges for both directions if directed
        graph
      * Refactor insert_vertex to require unique label
    
    Add dfs.py

--- 2021/d12p1.py
+++ 2021/d12p1.py
@@ -1,7 +1,6 @@
 #! /usr/bin/env python3.10
 
-from pprint import pprint
-
+from dfs import dfs, construct_path, dfs_complete
 from graph import Graph
 
 INFILE = 'd12p1t1.txt'
@@ -10,17 +9,63 @@ INFILE = 'd12p1t1.txt'
 # INFILE = 'd12p1.txt'
 
 
+def get_dfs_edges(dfs_res):
+    edge_path = 'Edge Path:  '
+    edges = len(dfs_res) - 1
+    for n, (key, value) in enumerate(dfs_res.items()):
+        if value:
+            edge_path += f'{value}'
+            if n < edges:
+                edge_path += ', '
+        else:
+            edge_path += f'from {key.label}>> '
+
+    return edge_path
+
+
 def main():
-    cave_graph = Graph()
+    cave_graph = Graph(directed=True)
+    start = 'start'
     with open(INFILE) as infile:
         for edge in infile:
-            start_vertex, end_vertex = edge.split('-')
-            # Check if already present?
-            sv = cave_graph.insert_vertex(start_vertex)
-            ev = cave_graph.insert_vertex(end_vertex)
-            cave_graph.insert_edge(sv, ev)
+            start_vertex, end_vertex = edge.strip().split('-')
+            if not (sv := cave_graph.get_vertex(start_vertex)):
+                sv = cave_graph.insert_vertex(start_vertex)
+            if not (ev := cave_graph.get_vertex(end_vertex)):
+                ev = cave_graph.insert_vertex(end_vertex)
+            if not cave_graph.get_edge(sv, ev):
+                cave_graph.insert_edge(sv, ev)
 
-    pprint(cave_graph.__dict__)
+    print(cave_graph)
+
+    '''
+    Need DFS-like approach or BFS-like approach
+    * Treat as directed graph, for edge set of edges add both directions
+    * From vertex 'start', find all possible edges
+    * What about collecting edge combinations from DFS starting from each
+      possible vertex?
+      * From 'start', recursively descend until get to 'end'
+      * Cannot go back to start (no cycles)
+      * Keep track of edge path - e.g., start->A, A->c, c->A, A->end
+        -or-
+      * Keep track of vertex path - e.g., start, A, c, end
+      * Keep track of visited edges - need all permutations of unique edge
+        combinations to get from 'start' to 'end'
+
+    * Some failed approaches:
+    * 1:  DFS gives a spanning tree, but not the solution for this challenge
+    if not (start_vert := cave_graph.get_vertex(start)):
+        raise ValueError('Expected a vertex labelled {start}')
+    dfs_res = dfs(cave_graph, start_vert)
+    print(get_dfs_edges(dfs_res))
+
+    * 2:  Only interested in starting from start vertex:
+    for vertex in cave_graph.vertices():
+        dfs_res = dfs(cave_graph, vertex)
+        print(get_dfs_edges(dfs_res))
+    '''
+    if not (start_vert := cave_graph.get_vertex(start)):
+        raise ValueError('Expected a vertex labelled {start}')
 
 
 if __name__ == '__main__':
--- 2021/graph.py
+++ 2021/graph.py
@@ -1,95 +1,136 @@
-# Copyright 2013, Michael H. Goldwasser
-#
-# Developed for use with the book:
-#
-#    Data Structures and Algorithms in Python
-#    Michael T. Goodrich, Roberto Tamassia, and Michael H. Goldwasser
-#    John Wiley & Sons, 2013
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
+'''
+Based on graph.py from:
+Data Structures and Algorithms in Python
+Michael T. Goodrich, Roberto Tamassia, and Michael H. Goldwasser
+John Wiley & Sons, 2013
+'''
 class Graph:
   """Representation of a simple graph using an adjacency map."""
 
-  #------------------------- nested Vertex class -------------------------
-  class Vertex:
-    """Lightweight vertex structure for a graph."""
-    __slots__ = '_element'
-  
-    def __init__(self, x):
-      """Do not call constructor directly. Use Graph's insert_vertex(x)."""
-      self._element = x
-  
-    def element(self):
-      """Return element associated with this vertex."""
-      return self._element
-  
-    def __hash__(self):         # will allow vertex to be a map/set key
-      return hash(id(self))
-
-    def __str__(self):
-      return str(self._element)
-    
-  #------------------------- nested Edge class -------------------------
-  class Edge:
-    """Lightweight edge structure for a graph."""
-    __slots__ = '_origin', '_destination', '_element'
-  
-    def __init__(self, u, v, x):
-      """Do not call constructor directly. Use Graph's insert_edge(u,v,x)."""
-      self._origin = u
-      self._destination = v
-      self._element = x
-  
-    def endpoints(self):
-      """Return (u,v) tuple for vertices u and v."""
-      return (self._origin, self._destination)
-  
-    def opposite(self, v):
-      """Return the vertex that is opposite v on this edge."""
-      if not isinstance(v, Graph.Vertex):
-        raise TypeError('v must be a Vertex')
-      return self._destination if v is self._origin else self._origin
-      raise ValueError('v not incident to edge')
-  
-    def element(self):
-      """Return element associated with this edge."""
-      return self._element
-  
-    def __hash__(self):         # will allow edge to be a map/set key
-      return hash( (self._origin, self._destination) )
-
-    def __str__(self):
-      return '({0},{1},{2})'.format(self._origin,self._destination,self._element)
-    
-  #------------------------- Graph methods -------------------------
   def __init__(self, directed=False):
     """Create an empty graph (undirected, by default).
 
     Graph is directed if optional paramter is set to True.
     """
     self._outgoing = {}
-    # only create second map for directed graph; use alias for undirected
+    # Only create second map for directed graph; use alias for undirected:
     self._incoming = {} if directed else self._outgoing
+
+    # Enforce unique labels for vertices:
+    self._vlabels = {}
+
+  def __repr__(self):
+    vertices = ', '.join(sorted(repr(k) for k in self.vertices()))
+    graph = '<Graph('
+    if self.is_directed():
+      graph += 'directed, '
+    graph += f'{vertices})>'
+
+    return graph
+
+  def __str__(self):
+    max_width = 6
+    directed = 'Directed Graph' if self.is_directed() else 'Undirected Graph'
+    vertices = self.format_output(self.vertices(), max_width)
+    edges = self.format_output(self.edges(), max_width - 1)
+    return f'{directed}\nVertices:  {vertices}\n   Edges:  {edges}'
 
   def _validate_vertex(self, v):
     """Verify that v is a Vertex of this graph."""
     if not isinstance(v, self.Vertex):
       raise TypeError('Vertex expected')
+
     if v not in self._outgoing:
       raise ValueError('Vertex does not belong to this graph.')
-    
+
+  def degree(self, v, outgoing=True):
+    """Return number of (outgoing) edges incident to vertex v in the graph.
+
+    If graph is directed, optional parameter used to count incoming edges.
+    """
+    self._validate_vertex(v)
+    adj = self._outgoing if outgoing else self._incoming
+    return len(adj[v])
+
+  def edge_count(self):
+    """Return the number of edges in the graph."""
+    total = sum(len(self._outgoing[v]) for v in self._outgoing)
+    # for undirected graphs, make sure not to double-count edges
+    return total if self.is_directed() else total // 2
+
+  def edges(self):
+    """Return a set of all edges of the graph."""
+    result = set()       # avoid double-reporting edges of undirected graph
+    # Add edges to result set:
+    for vertex_out in self._outgoing.values():
+      result.update(vertex_out.values())
+
+    if self.is_directed():
+      for vertex_in in self._incoming.values():
+        result.update(vertex_in.values())
+
+    return result
+
+  def format_output(self, values, max_width=6):
+    raw_values = sorted(repr(k) for k in values)
+    fmt_values = ''
+    if (values := len(raw_values)) > max_width:
+      for cur_val in range(0, values, max_width):
+        end_val = min(cur_val + max_width, values)
+        fmt_values += ', '.join(raw_values[cur_val:end_val])
+        if end_val != values:
+          fmt_values += '\n           '
+    else:
+      fmt_values = ', '.join(raw_values)
+
+    return fmt_values
+
+  def get_edge(self, u, v):
+    """Return the edge from u to v, or None if not adjacent."""
+    self._validate_vertex(u)
+    self._validate_vertex(v)
+    return self._outgoing[u].get(v)        # returns None if v not adjacent
+
+  def get_vertex(self, label):
+    return self._vlabels.get(label)
+
+  def incident_edges(self, v, outgoing=True):
+    """Return all (outgoing) edges incident to vertex v in the graph.
+
+    If graph is directed, optional parameter used to request incoming edges.
+    """
+    self._validate_vertex(v)
+    adj = self._outgoing if outgoing else self._incoming
+    yield from adj[v].values()
+
+  def insert_edge(self, u, v, label=None):
+    """Insert and return a new Edge from u to v with auxiliary label.
+
+    Raise a ValueError if u and v are not vertices of the graph.
+    Raise a ValueError if u and v are already adjacent.
+    """
+    if self.get_edge(u, v) is not None:      # includes error checking
+      raise ValueError('u and v are already adjacent')
+
+    e_out = self.Edge(u, v, label)
+    e_in = self.Edge(v, u, label) if self.is_directed() else e_out
+
+    self._outgoing[u][v] = e_out
+    self._incoming[v][u] = e_in
+
+  def insert_vertex(self, label):
+    """Insert and return a new Vertex with label."""
+    if label in self._vlabels:
+      raise KeyError('Vertex label must be unique')
+
+    v = self.Vertex(label)
+    self._vlabels[label] = v
+    self._outgoing[v] = {}
+    if self.is_directed():
+      self._incoming[v] = {}        # need distinct map for incoming edges
+
+    return v
+
   def is_directed(self):
     """Return True if this is a directed graph; False if undirected.
 
@@ -105,60 +146,84 @@ class Graph:
     """Return an iteration of all vertices of the graph."""
     return self._outgoing.keys()
 
-  def edge_count(self):
-    """Return the number of edges in the graph."""
-    total = sum(len(self._outgoing[v]) for v in self._outgoing)
-    # for undirected graphs, make sure not to double-count edges
-    return total if self.is_directed() else total // 2
+  class Edge:
+    """Lightweight edge structure for a graph."""
 
-  def edges(self):
-    """Return a set of all edges of the graph."""
-    result = set()       # avoid double-reporting edges of undirected graph
-    for secondary_map in self._outgoing.values():
-      result.update(secondary_map.values())    # add edges to resulting set
-    return result
+    # Edge label optional:
+    def __init__(self, u, v, label=''):
+      """Do not call constructor directly. Use Graph's insert_edge(u, v, label)."""
+      self._origin = u
+      self._destination = v
+      self._label = label
 
-  def get_edge(self, u, v):
-    """Return the edge from u to v, or None if not adjacent."""
-    self._validate_vertex(u)
-    self._validate_vertex(v)
-    return self._outgoing[u].get(v)        # returns None if v not adjacent
+    def endpoints(self):
+      """Return (u, v) tuple for vertices u and v."""
+      return (self._origin, self._destination)
 
-  def degree(self, v, outgoing=True):   
-    """Return number of (outgoing) edges incident to vertex v in the graph.
+    def opposite(self, v):
+      """Return the vertex that is opposite v on this edge."""
+      if not isinstance(v, Graph.Vertex):
+        raise TypeError('v must be a Vertex')
 
-    If graph is directed, optional parameter used to count incoming edges.
-    """
-    self._validate_vertex(v)
-    adj = self._outgoing if outgoing else self._incoming
-    return len(adj[v])
+      if v is self._destination:
+        return self._origin
+      elif v is self._origin:
+        return self._destination
+      else:
+        raise ValueError(f'v not incident to edge({self._label})')
 
-  def incident_edges(self, v, outgoing=True):   
-    """Return all (outgoing) edges incident to vertex v in the graph.
+    @property
+    def label(self):
+      """Return label associated with this edge."""
+      return self._label
 
-    If graph is directed, optional parameter used to request incoming edges.
-    """
-    self._validate_vertex(v)
-    adj = self._outgoing if outgoing else self._incoming
-    for edge in adj[v].values():
-      yield edge
+    @label.setter
+    def label(self, value):
+      self._label = value
 
-  def insert_vertex(self, x=None):
-    """Insert and return a new Vertex with element x."""
-    v = self.Vertex(x)
-    self._outgoing[v] = {}
-    if self.is_directed():
-      self._incoming[v] = {}        # need distinct map for incoming edges
-    return v
-      
-  def insert_edge(self, u, v, x=None):
-    """Insert and return a new Edge from u to v with auxiliary element x.
+    @label.deleter
+    def label(self):
+      self._label = ''
 
-    Raise a ValueError if u and v are not vertices of the graph.
-    Raise a ValueError if u and v are already adjacent.
-    """
-    if self.get_edge(u, v) is not None:      # includes error checking
-      raise ValueError('u and v are already adjacent')
-    e = self.Edge(u, v, x)
-    self._outgoing[u][v] = e
-    self._incoming[v][u] = e
+    # Allow vertex to be a map/set key:
+    def __hash__(self):
+      return hash((self._origin, self._destination))
+
+    def __repr__(self):
+      connstr = f'<==({self._label})==>' if self._label else '<==>'
+      return f'<Edge({self._origin}{connstr}{self._destination})>'
+
+    def __str__(self):
+      connstr = f'<==({self._label})==>' if self._label else '<==>'
+      return f'({self._origin}{connstr}{self._destination})'
+
+  class Vertex:
+    """Lightweight vertex structure for a graph."""
+
+    # Require label for a vertex:
+    def __init__(self, label):
+      """Do not call constructor directly. Use Graph's insert_vertex(label)."""
+      self._label = label
+
+    @property
+    def label(self):
+      """Return label associated with this vertex."""
+      return self._label
+
+    @label.setter
+    def label(self, value):
+      self._label = value
+
+    @label.deleter
+    def label(self):
+      self._label = ''
+
+    # Allow vertex to be a map/set key:
+    def __hash__(self):
+      return hash(id(self))
+
+    def __repr__(self):
+      return f'<Vertex({self._label})>'
+
+    def __str__(self):
+      return str(self._label)
