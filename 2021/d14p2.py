2f21c56c1f49d216869c206211995e4c6bb53d5a
James Small <james.r.small@outlook.com>
2022-02-01 22:52:14 -0500

    Update:  Continue working on day 14, part 2 code challenge

--- 2021/d14p2.py
+++ 2021/d14p2.py
@@ -11,16 +11,39 @@ INFILE = 'd14p1t1.txt'
 
 '''
 Need more efficient approach:
-* There are 4 elements
+* There are 4 - 10 elements (can encode as 0 - 9)
 * Rather than encoding as UTF-8 or worse, encode as int or something more efficient
+  * Map each element to a digit
+
 * Encode in a way that makes easy to update - perhaps favor encoding over printing or
   displaying efficiently
 * When step through to apply insertion rules, need more efficient updating approach
+  * Group insertion rules - set that results in element 1, set that results in element
+    2, ...
 '''
 class Polymer():
     def __init__(self, ptmpl, insrules):
         self.ptmpl = ptmpl
         self.insrules = insrules
+
+        # Fast map from old pair to new pairs:
+        self.xformrules = {key: (key[0] + val, val + key[1]) for key, val in insrules.items()}
+
+        # Convert polymer template into pairs (keys) where value is counter
+        self.polymer = {key: 0 for key in insrules}
+
+        for p1, p2 in pairwise(self.ptmpl):
+            self.polymer[p1 + p2] += 1
+
+        # Convert to more efficient representations:
+        '''
+        elements = set(self.ptmpl) | set(self.insrules.values())
+        self.ptmpl_map = dict(enumerate(elements))
+        self.insrules_map = {
+            frozenset({key for key, val in self.insrules.items() if val == e}): e
+            for e in elements
+        }
+        '''
 
     def __repr__(self):
         ptmpl_len = len(self.ptmpl)
@@ -29,7 +52,7 @@ class Polymer():
         insrules = ', '.join(f'{a}=>{b}' for a, b in islice(self.insrules.items(), 4))
         if insrules_len > 4:
             insrules += '...'
-        return f'<Polymer({ptmpl} ({ptmpl_len}), {insrules} ({insrules_len}))>'
+        return f'<Polymer({ptmpl} ({ptmpl_len:,}), {insrules} ({insrules_len}))>'
 
     def __str__(self):
         max_width = 80
@@ -41,7 +64,7 @@ class Polymer():
         insrules = ', '.join(f'{a}=>{b}' for a, b in islice(self.insrules.items(), max_items))
         if insrules_len > max_items:
             insrules += '...'
-        return (f'    Polymer Template:  {ptmpl} ({ptmpl_len})\n'
+        return (f'    Polymer Template:  {ptmpl} ({ptmpl_len:,})\n'
                 f'Pair Insertion Rules:  {insrules} ({insrules_len})')
 
     def diff(self, verbose=True):
@@ -52,13 +75,26 @@ class Polymer():
         return max(vals) - min(vals)
 
     def step(self):
-        ptmpl = ''
-        for p1, p2 in pairwise(self.ptmpl):
-            key = p1 + p2
-            mid = self.insrules[key]
-            ptmpl = p1 + mid + p2 if not ptmpl else ptmpl + mid + p2
+        # ptmpl = self.ptmpl[:1]
+        # for p1, p2 in pairwise(self.ptmpl):
+        # for start, stop in enumerate(range(2, len(self.ptmpl) + 1)):
+        '''
+        for top in range(2, len(self.ptmpl) + 1):
+            val = self.ptmpl[top - 2:top]
+            ptmpl += self.insrules[val] + val[1]
 
         self.ptmpl = ptmpl
+        '''
+        polymer = {key: 0 for key in self.insrules}
+        for key, val in self.polymer.items():
+            if val:
+                key1, key2 = self.xformrules[key]
+                polymer[key1] += val
+                polymer[key2] += val
+
+        self.polymer = polymer
+
+        # self.ptmpl = ''.join(key * val for key, val in polymer.items())
 
 
 def main(verbose=True):
@@ -80,11 +116,12 @@ def main(verbose=True):
 
     # Works for 10 steps - fails for 40, believe n^2 growth, need better
     # approach:
-    for n in range(1, 41):
+    for n in range(1, 11):
         polymer.step()
         if verbose:
             print(f'\n                Step:  {n}\n{polymer}')
-            print(f'\nDifference:  {polymer.diff()}')
+            # print(f'\nDifference:  {polymer.diff()}')
+            print(f'Polymer:  {polymer.polymer}')
 
     print(f'\nDifference:  {polymer.diff()}')
 
