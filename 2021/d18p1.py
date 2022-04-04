9e2d07f27b0e3c34a158f3dabea9806af74b83c3
James Small <james.r.small@outlook.com>
2022-04-03 20:34:34 -0400

    Update:  Continue working on day 18, part 1 code challenge

--- 2021/d18p1.py
+++ 2021/d18p1.py
@@ -35,6 +35,110 @@ class SnailfishNumber():
         self._reduce()
         return self
 
+    def _explode(self, poslist):
+        '''
+        Explode SnailfishNumber:
+        To explode a pair, the pair's left value is added to the first regular number to the
+        left of the exploding pair (if any), and the pair's right value is added to the first
+        regular number to the right of the exploding pair (if any). Exploding pairs will always
+        consist of two regular numbers. Then, the entire exploding pair is replaced with the
+        regular number 0.
+        '''
+        cur = poslist.first()
+        prev = None
+        depth = 0
+        explode = False
+        while cur:
+            match cur.element():
+                case '[':
+                    depth += 1
+                    if depth >= 5 and not explode:
+                        explode = True
+                case ']':
+                    depth -= 1
+                case num if isinstance(num, int):
+                    if explode and prev and isinstance(prev.element(), int):
+                        # Look for number to the left:
+                        leftval, leftcur = prev.element(), prev
+                        prevleft = leftcur
+                        while (prevleft := poslist.before(prevleft)):
+                            if isinstance((lnum := prevleft.element()), int):
+                                leftval += lnum
+                                poslist.replace(prevleft, leftval)
+                                break
+                        poslist.delete(poslist.before(leftcur))
+                        poslist.delete(leftcur)
+
+                        # Look for number to the right:
+                        rightval, rightcur = num, cur
+                        nextright = rightcur
+                        while (nextright := poslist.after(nextright)):
+                            if isinstance((rnum := nextright.element()), int):
+                                rightval += rnum
+                                poslist.replace(nextright, rightval)
+                                break
+                        poslist.delete(poslist.after(rightcur))
+                        poslist.replace(rightcur, 0)
+
+                        # Immediately return after each explode
+                        # depth -= 1
+                        # explode = False
+                        break
+            prev = cur
+            cur = poslist.after(cur)
+
+        return poslist, explode
+
+    def _split(self, poslist):
+        '''
+        Split SnailfishNumber:
+        To split a regular number, replace it with a pair; the left element of the pair should
+        be the regular number divided by two and rounded down, while the right element of the
+        pair should be the regular number divided by two and rounded up. For example, 10 becomes
+        [5,5], 11 becomes [5,6], 12 becomes [6,6], and so on.
+        '''
+        ### To implement...
+        cur = poslist.first()
+        prev = None
+        split = False
+        while cur:
+            match cur.element():
+                case '[':
+                    ...
+                case ']':
+                    ...
+                case num if isinstance(num, int):
+                    if split and prev and isinstance(prev.element(), int):
+                        # Look for number to the left:
+                        leftval, leftcur = prev.element(), prev
+                        prevleft = leftcur
+                        while (prevleft := poslist.before(prevleft)):
+                            if isinstance((lnum := prevleft.element()), int):
+                                leftval += lnum
+                                poslist.replace(prevleft, leftval)
+                                break
+                        poslist.delete(poslist.before(leftcur))
+                        poslist.delete(leftcur)
+
+                        # Look for number to the right:
+                        rightval, rightcur = num, cur
+                        nextright = rightcur
+                        while (nextright := poslist.after(nextright)):
+                            if isinstance((rnum := nextright.element()), int):
+                                rightval += rnum
+                                poslist.replace(nextright, rightval)
+                                break
+                        poslist.delete(poslist.after(rightcur))
+                        poslist.replace(rightcur, 0)
+
+                        # Immediately return after each split
+                        # split = False
+                        break
+            prev = cur
+            cur = poslist.after(cur)
+
+        return poslist, split
+
     def _reduce(self):
         '''
         Repeatedly:
@@ -45,67 +149,17 @@ class SnailfishNumber():
                there are no explodes and two splits, only one split occurs before checking for an
                explode again.
 
-        To explode a pair, the pair's left value is added to the first regular number to the
-        left of the exploding pair (if any), and the pair's right value is added to the first
-        regular number to the right of the exploding pair (if any). Exploding pairs will always
-        consist of two regular numbers. Then, the entire exploding pair is replaced with the
-        regular number 0.
-
-        To split a regular number, replace it with a pair; the left element of the pair should
-        be the regular number divided by two and rounded down, while the right element of the
-        pair should be the regular number divided by two and rounded up. For example, 10 becomes
-        [5,5], 11 becomes [5,6], 12 becomes [6,6], and so on.
-
         *** Next Steps
         Leverage positional list ADT from Python DSA book for the task below:
         '''
-        # Build out explode procedure...
+        explode = True
+        split = True
         poslist = self._poslist()
-        cur = poslist.first()
-        left_stack = []
-        left = True
-        depth = 0
-        explode = False
-        split = False
-        while cur:
-            match cur.element():
-                case '[':
-                    depth += 1
-                    left = True
-                    if depth >= 4 and not explode:
-                        explode = True
-                        print('Explode next pair!')
-                case ']':
-                    depth -= 1
-                case num if isinstance(num, int):
-                    if left:
-                        left_stack.append((num, cur))
-                        left = False
-                    else:
-                        if explode:
-                            print('Exploding...')
-                            leftval, leftcur = left_stack[-1]
-                            poslist.delete(poslist.before(leftcur))
-                            poslist.delete(leftcur)
-                            if len(left_stack) > 1:
-                                leftval += left_stack[-2][0]
-                                poslist.replace(left_stack[-2][1], leftval)
-                            rightval, rightcur = num, cur
-                            nextright = rightcur
-                            while (nextright := poslist.after(nextright)):
-                                if isinstance((rnum := nextright.element()), int):
-                                    rightval += rnum
-                                    poslist.replace(nextright, rightval)
-                                    break
-                            poslist.delete(poslist.after(rightcur))
-                            poslist.replace(rightcur, 0)
 
-                            explode = False
-                case ',':
-                    left = False if left else True
-            cur = poslist.after(cur)
+        # while explode or split:
+        poslist, explode = self._explode(poslist)
+            # poslist, split = self._split(poslist)
 
-        print(f'Exploded positional List:  {poslist}')
         self.number = json.loads(poslist.to_json())
         return self.number
 
@@ -116,16 +170,9 @@ class SnailfishNumber():
             match res.group(1):
                 case '[':
                     poslist.add_last('[')
-                    '''
-                    depth += 1
-                    if depth >= 4 and not explode:
-                        explode = True
-                        print('Explode next pair!')
-                    '''
                 case ']':
                     poslist.add_last(']')
-                    ### depth -= 1
-                case num if num[0] in '123456789':
+                case num if num[0] in '0123456789':
                     poslist.add_last(int(num))
                 case ',':
                     pass
