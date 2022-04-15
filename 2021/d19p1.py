04b7345d27be77765c2ff85ec39e25ecba7a2f6a
James Small <james.r.small@outlook.com>
2022-04-15 10:17:11 -0400

    Update:  Continue working on day 19, part 1 code challenge

--- 2021/d19p1.py
+++ 2021/d19p1.py
@@ -15,6 +15,10 @@ import re
 from graph import Graph
 
 
+# Globals:
+MAX_DIST = sqrt( 1_000**2 + 1_000**2 + 1_000**2 )  # Approx: 1,732.0508
+
+
 class Scanner():
     def __init__(self, id, beacons=None):
         self.id = id
@@ -69,7 +73,10 @@ def cmp_vertices(scanner1, scanner2):
     Next step:  From matching edges, pull out all vertices and find unique set
     '''
     matching_edges = []
-    max_dist = 1_000
+    s1_edges = []
+    s2_edges = []
+    # max_dist = 1_000
+    max_dist = MAX_DIST
 
     edges1 = sorted(scanner1.graph.edges())
     edges2 = sorted(scanner2.graph.edges())
@@ -83,6 +90,8 @@ def cmp_vertices(scanner1, scanner2):
 
         if isclose(edge1.label, edge2.label):
             if edge1.label <= max_dist:
+                s1_edges.append(edge1)
+                s2_edges.append(edge2)
                 matching_edges.append((edge1, edge2))
             edge1 = None
             edge2 = None
@@ -91,7 +100,14 @@ def cmp_vertices(scanner1, scanner2):
         else:
             edge2 = None
 
-    return matching_edges
+    s1_vertices = set()
+    for edge in s1_edges:
+        s1_vertices.update(edge.endpoints())
+    s2_vertices = set()
+    for edge in s2_edges:
+        s2_vertices.update(edge.endpoints())
+
+    return s1_vertices, s2_vertices, matching_edges
 
 
 def get_vert_edges(scanner):
@@ -167,12 +183,17 @@ def main():
     same_verts = {k: [] for k in combinations(scanners, 2)}
     for key in same_verts:
         s1, s2 = key
-        if res := cmp_vertices(s1, s2):
+        s1_vertices, s2_vertices, res = cmp_vertices(s1, s2)
+        if res:
             print(f'For scanner {s1.id} and scanner {s2.id} found {len(res)} matching edges.')
+            print(f'Scanner {s1.id} overlapping vertices:\n{[str(i) for i in s1_vertices]}')
+            print(f'Scanner {s2.id} overlapping vertices:\n{[str(i) for i in s2_vertices]}')
+            pprint(res)
+            print()
             same_verts[key].extend(res)
 
-    print(f'Found {len(same_verts)} matching vertices:')
-    pprint(same_verts)
+    ### print(f'Found {len(same_verts)} matching vertices:')
+    ### pprint(same_verts)
 
     # Temporary:
     return scanners
