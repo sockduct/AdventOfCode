a0128d81742aa1bfeafc817da5bd410d1b263850
James Small <james.r.small@outlook.com>
2022-04-17 00:49:10 -0400

    Update:  Continue working on day 19, part 1 code challenge

--- 2021/d19p1.py
+++ 2021/d19p1.py
@@ -6,6 +6,7 @@ INFILE = 'd19p1t1.txt'
 
 # Libraries
 # Standard Library:
+from collections import defaultdict
 from itertools import combinations
 from math import isclose, sqrt
 from pprint import pprint
@@ -68,9 +69,6 @@ class Scanner():
 def cmp_vertices(scanner1, scanner2):
     '''
     Take two scanners (graph portion) and return matching edges <= max_dist
-
-    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    Next step:  From matching edges, pull out all vertices and find unique set
     '''
     matching_edges = []
     s1_edges = []
@@ -183,14 +181,74 @@ def main():
     same_verts = {k: [] for k in combinations(scanners, 2)}
     for key in same_verts:
         s1, s2 = key
-        s1_vertices, s2_vertices, res = cmp_vertices(s1, s2)
-        if res:
-            print(f'For scanner {s1.id} and scanner {s2.id} found {len(res)} matching edges.')
-            print(f'Scanner {s1.id} overlapping vertices:\n{[str(i) for i in s1_vertices]}')
-            print(f'Scanner {s2.id} overlapping vertices:\n{[str(i) for i in s2_vertices]}')
-            pprint(res)
+        s1_vertices, s2_vertices, edges = cmp_vertices(s1, s2)
+        if edges:
+            print(f'\n\nFor scanner {s1.id} and scanner {s2.id} found {len(edges)} matching edges.')
+            print(f'\nScanner {s1.id} overlapping vertices:\n{[str(i) for i in s1_vertices]}')
+            print(f'\nScanner {s2.id} overlapping vertices:\n{[str(i) for i in s2_vertices]}\n')
+            # pprint(edges)
             print()
-            same_verts[key].extend(res)
+            same_verts[key].extend(edges)
+
+        s1_verts = {vert: defaultdict(int) for vert in s1_vertices}
+        s2_verts = {vert: defaultdict(int) for vert in s2_vertices}
+        for e1, e2 in edges:
+            s1_v1, s1_v2 = e1.endpoints()
+            s2_v1, s2_v2 = e2.endpoints()
+            s1_verts[s1_v1][s2_v1] += 1
+            s1_verts[s1_v1][s2_v2] += 1
+            s1_verts[s1_v2][s2_v1] += 1
+            s1_verts[s1_v2][s2_v2] += 1
+            s2_verts[s2_v1][s1_v1] += 1
+            s2_verts[s2_v1][s1_v2] += 1
+            s2_verts[s2_v2][s1_v1] += 1
+            s2_verts[s2_v2][s1_v2] += 1
+
+        #pprint(s1_verts)
+        #pprint(s2_verts)
+
+        # With this having matching vertices
+        s1_verts_equiv = {}
+        for base_key, base_values in s1_verts.items():
+            matched_nested = False
+            for nested_key, nested_value in base_values.items():
+                if nested_value == 11 and not matched_nested:
+                    matched_nested = True
+                    s1_verts_equiv[base_key] = nested_key
+                elif nested_value ==11 and matched_nested:
+                    raise ValueError('Matched 11 more than once...')
+
+        '''
+        ### Next Step:
+        * Figure out scanning rotation/facing direction by using 12 matching
+          beacons
+
+          Each scanner is rotated some integer number of 90-degree turns around all
+          of the x, y, and z axes. That is, one scanner might call a direction positive
+          x, while another scanner might call that direction negative y. Or, two scanners
+          might agree on which direction is positive x, but one scanner might be upside-down
+          from the perspective of the other scanner. In total, each scanner could be in
+          any of 24 different orientations: facing positive or negative x, y, or z, and
+          considering any of four directions "up" from that facing.
+        '''
+
+        '''
+        # These are mirror image of above and not necessary:
+        s2_verts_equiv = {}
+        for base_key, base_values in s2_verts.items():
+            matched_nested = False
+            for nested_key, nested_value in base_values.items():
+                if nested_value == 11 and not matched_nested:
+                    matched_nested = True
+                    s2_verts_equiv[base_key] = nested_key
+                elif nested_value ==11 and matched_nested:
+                    raise ValueError('Matched 11 more than once...')
+        '''
+
+        pprint(s1_verts_equiv)
+        #pprint(s2_verts_equiv)
+        # Start with just 0 and 1:
+        break
 
     ### print(f'Found {len(same_verts)} matching vertices:')
     ### pprint(same_verts)
