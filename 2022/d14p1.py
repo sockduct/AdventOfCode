8a39b8b1a51bd8628f232e382a944e897f746bc0
James Small <james.r.small@outlook.com>
2022-12-28 22:59:19 -0500

    Update:  Continue working on day 14, part 1 challenge

--- 2022/d14p1.py
+++ 2022/d14p1.py
@@ -7,6 +7,7 @@ Regolith Reservoir
   * Scan data shows path of rock structures (x, y)
     * x represents distance to the right
     * y represents distance down
+  * Sand movement - see drop_sand
 '''
 
 
@@ -32,6 +33,13 @@ class Point:
     def __str__(self):
         return f'{self.x, self.y}'
 
+    def __add__(self, other):
+        return (
+            Point(self.x + other.x, self.y + other.y)
+                if isinstance(other, self.__class__)
+                else NotImplemented
+        )
+
     def __sub__(self, other):
         return (
             Point(self.x - other.x, self.y - other.y)
@@ -52,26 +60,30 @@ class Point:
         )
 
 
+def calc_offsets(bounds):
+    bounds['width'] = bounds['right'].x - bounds['left'].x
+    bounds['woff1'] = bounds['sand_orig'].x - bounds['left'].x
+    bounds['woff2'] = bounds['right'].x - bounds['sand_orig'].x
+    bounds['voff'] = 3
+    bounds['hoff'] = 4
+
+
 def build_graph(point_lines, bounds):
-    width = bounds['right'].x - bounds['left'].x
-    woff1 = bounds['sand_orig'].x - bounds['left'].x
-    woff2 = bounds['right'].x - bounds['sand_orig'].x
-    hoff = 3
-    voff = 4
-
+    # Create numbers at top of graph for x-axis:
     outlines = [
-        (f"{' ' * voff}{str(bounds['left'].x)[i]}{' ' * (woff1 - 1)}"
-         f"{str(bounds['sand_orig'].x)[i]}{' ' * (woff2 - 1)}"
-         f"{str(bounds['right'].x)[i]}")
-            for i in range(hoff)
+        list(f"{' ' * bounds['hoff']}{str(bounds['left'].x)[i]}{' ' * (bounds['woff1'] - 1)}"
+             f"{str(bounds['sand_orig'].x)[i]}{' ' * (bounds['woff2'] - 1)}"
+             f"{str(bounds['right'].x)[i]}")
+                for i in range(bounds['voff'])
     ]
+    # Create numbers at the side of graph for y-axis and "air":
     outlines.extend(
-        f"{i:3} {'.' * (width + 1)}" for i in range(bounds['lower'].y + 1)
+        list(f"{i:3} {AIR * (bounds['width'] + 1)}") for i in range(bounds['lower'].y + 1)
     )
+    # Draw sand source:
+    outlines[bounds['voff']][bounds['hoff'] + bounds['woff1']] = SANDSRC
 
-    newline = outlines[hoff][:voff + woff1] + SANDSRC + outlines[hoff][voff + woff1:-1]
-    outlines[hoff] = newline
-
+    # Draw in lines of rock using coordinates:
     for line in point_lines:
         for c1, c2 in pairwise(line):
             c12 = c1.offset(c2)
@@ -81,20 +93,54 @@ def build_graph(point_lines, bounds):
                 right = c2.x if c2 > c1 else c1.x
                 xoff1 = left - bounds['left'].x
                 xoff2 = right - bounds['left'].x
-                outlines[hoff + c1.y] = (
-                    outlines[hoff + c1.y][:voff + xoff1] + ROCK * c12.x +
-                    outlines[hoff + c1.y][voff + xoff2:]
-                )
+                for xoff in range(xoff1, xoff2 + 1):
+                    outlines[bounds['voff'] + c1.y][bounds['hoff'] + xoff] = ROCK
             else:
                 # Y-offset
                 xoff = c1.x - bounds['left'].x
                 for yoff in range(c1.y, c2.y + 1):
-                    outlines[hoff + yoff] = (
-                        outlines[hoff + yoff][:voff + xoff] + ROCK +
-                        outlines[hoff + yoff][voff + xoff:-1]
-                    )
+                    outlines[bounds['voff'] + yoff][bounds['hoff'] + xoff] = ROCK
 
     return outlines
+
+
+def display(graph):
+    print()
+    for line in graph:
+        print(''.join(line))
+    print()
+
+
+def drop_sand(graph, bounds):
+    '''
+    Sand movement:
+    0) Originates from 500, 0
+    1) Goes down one step if possible (nothing in the way)
+    2) If can't go down, tries to go diagonally left
+    3) If can't go diagonally left, tries to go diagonally right
+    *) If can't do any of of 1-3, then stops, and next unit starts (step 0)
+    '''
+    # Start 1 below Sand Origin:
+    row = bounds['voff'] + 1
+    col = bounds['hoff'] + bounds['woff1']
+
+    while True:
+        match graph[row][col]:
+            # AIR:
+            case '.':
+                row += 1
+            # ROCK:
+            case '#':
+                # Check diagonally left:
+                if graph[row][col - 1] == '.':
+                    col -= 1
+                # Check diagonally right:
+                elif graph[row][col + 1] == '.':
+                    col += 1
+                # Nowhere to go - stop here:
+                else:
+                    graph[row - 1][col] = 'o'
+                    break
 
 
 def main(verbose=True):
@@ -128,16 +174,17 @@ def main(verbose=True):
         print(f"\nLeft bound:  {bounds['left']}\nRight bound:  {bounds['right']}\n"
               f"Lower bound:  {bounds['lower']}")
 
+    calc_offsets(bounds)
     graph = build_graph(point_lines, bounds)
 
     if verbose:
-        print()
-        for line in graph:
-            print(line)
-        print()
+        display(graph)
 
     # Next step - iterate through drawing sand, see AoC 2022, Day 14 overview
-    ...
+    drop_sand(graph, bounds)
+
+    if verbose:
+        display(graph)
 
 
 if __name__ == '__main__':
