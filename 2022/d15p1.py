abeefdc48aec34f1b1a9953057fbc155d8992fd8
James Small <james.r.small@outlook.com>
2023-01-03 21:13:57 -0500

    Add:  Begin working on day 15, part 1 code challenge

---
+++ 2022/d15p1.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python3
+
+
+'''
+Beacon Exclusion Zone:
+* Sensors at integer coordinates
+  * Knows its own position
+  * Can determine position of beacon closest to it as measured by the
+    manhattan distance
+  * There is never a tie where two beacons are the same distance from a sensor
+* Beacons at integer coordinates
+'''
+
+
+INFILE='d15p1t1.txt'
+# INFILE='d15p1.txt'
+
+
+def main():
+    ...
+
+
+if __name__ == '__main__':
+    main()
