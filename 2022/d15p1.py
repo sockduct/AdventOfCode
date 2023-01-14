d78b83fb05ae444e0c7cab4ae1b67c72f605741f
James Small <james.r.small@outlook.com>
2023-01-13 22:51:02 -0500

    Update:  Complete day 15 part 1 code challenge

--- 2022/d15p1.py
+++ 2022/d15p1.py
@@ -25,9 +25,41 @@ sys.path.append(LOCAL_LIB)
 from ds.grid import Grid, Point
 
 
-# INFILE = 'd15p1t1.txt'
-INFILE = Path(__file__).parent/'d15p1t1.txt'
-# INFILE = 'd15p1.txt'
+# INFILE = Path(__file__).parent/'d15p1t1.txt'
+INFILE = Path(__file__).parent/'d15p1.txt'
+
+
+def find_covered(xmin, xmax, y, sensors, verbose=False):
+    covered = 0
+
+    if verbose:
+        print(f'Examining range {xmin:,} - {xmax:,}...', end='', flush=True)
+        xrange = abs(xmax - xmin)
+        xoff = abs(xmin) if xmin < 0 else 0
+        xlen = 0
+        back = ''
+
+    for x in range(xmin, xmax + 1):
+        if verbose and x % 10_000 == 0:
+            if xlen > 0:
+                back = '\b' * xlen
+            xpct = round(((x + xoff)/xrange) * 100)
+            xlen = len(str(xpct)) + 1
+            print(f'{back}{xpct}%', end='', flush=True)
+        p = Point(x, y)
+        for sensor, beaconinfo in sensors.items():
+            beacon, beacondist = beaconinfo
+            if sensor == p or beacon == p:
+                # Don't count points occupied by sensor or beacon:
+                break
+            if sensor.mandist(p) <= beacondist:
+                covered += 1
+                break
+
+    if verbose:
+        print()
+
+    return covered
 
 
 def main(verbose=True):
@@ -50,27 +82,20 @@ def main(verbose=True):
             sensors[sensor] = (beacon, mandist)
 
             # Find corners of grid including accounting for "manhattan distance
-            # radius" around each point:
-            if sx < minx or bx < minx or (sx - mandist) < minx or (bx - mandist) < minx:
-                minx = min((sx, bx, sx - mandist, bx - mandist))
-            if sx > maxx or bx > maxx or (sx + mandist) > maxx or (bx + mandist) > maxx:
-                maxx = max((sx, bx, sx + mandist, bx + mandist))
-            if sy < miny or by < miny or (sy - mandist) < miny or (by - mandist) < miny:
-                miny = min((sy, by, sy - mandist, by - mandist))
-            if sy > maxy or by > maxy or (sy + mandist) > maxy or (by + mandist) > maxy:
-                maxy = max((sy, by, sy + mandist, by + mandist))
+            # radius" around each sensor (but not beacon - radius around sensor
+            # covers beacon):
+            if sx < minx or bx < minx or (sx - mandist) < minx:
+                minx = min((sx, bx, sx - mandist))
+            if sx > maxx or bx > maxx or (sx + mandist) > maxx:
+                maxx = max((sx, bx, sx + mandist))
+            if sy < miny or by < miny or (sy - mandist) < miny:
+                miny = min((sy, by, sy - mandist))
+            if sy > maxy or by > maxy or (sy + mandist) > maxy:
+                maxy = max((sy, by, sy + mandist))
 
-    grid = Grid(Point(minx, miny), Point(maxx, maxy))
-    for sensor, beaconinfo in sensors.items():
-        beacon, mandist = beaconinfo
-        grid.plot(sensor, 'S')
-        grid.plot(beacon, 'B')
-        grid.circle(sensor, mandist, '#', overwrite=False)
-
-    # Retrieve grid row 10:
-    row = grid.getrow(10)
-    cantbe = row.count('#')
-    print(f"\nRow 10:\n{row}\nNumber of positions where beacon can't be:  {cantbe:,}\n")
+    if verbose:
+        print('Input List - Sensor: Closest Beacon:')
+        pprint(sensors, sort_dicts=False)
 
     '''
     # Calculate manhattan distance between sensor at (8, 7) and its beacon at
@@ -82,11 +107,31 @@ def main(verbose=True):
     grid.circle(sensor, sensor.mandist(beacon), '#', overwrite=False)
     '''
 
-    if verbose:
-        print('Input List - Sensor: Closest Beacon:')
-        pprint(sensors, sort_dicts=False)
+    gridmin = Point(minx, miny)
+    gridmax = Point(maxx, maxy)
+
+    '''
+    grid = Grid(Point(minx, miny), Point(maxx, maxy))
+
+    for sensor, beaconinfo in sensors.items():
+        beacon, mandist = beaconinfo
+        grid.plot(sensor, 'S')
+        grid.plot(beacon, 'B')
+        grid.circle(sensor, mandist, '#', overwrite=False)
 
     print(f'\nPlot of sensors and beacons:\n{grid}')
+    '''
+
+    # Retrieve grid row target:
+    # row_target = 10  # Example problem input
+    row_target = 2_000_000
+    '''
+    row = grid.getrow(row_target)
+    cantbe = row.count('#')
+    print(f"\nRow {row_target}:\n{row}\nNumber of positions where beacon can't be:  {cantbe:,}\n")
+    '''
+    res = find_covered(gridmin.x, gridmax.x, row_target, sensors, verbose=True)
+    print(f"\nRow {row_target:,} - number of positions where beacon can't be:  {res:,}\n")
 
 
 if __name__ == '__main__':
