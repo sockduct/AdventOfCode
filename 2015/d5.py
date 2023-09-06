3e378c3d945b81755ee9be47813552c8b342a81f
James Small <james.r.small@outlook.com>
2023-09-06 08:14:43 -0400

    Update:  Begin day 5, part 2 code challenge

--- 2015/d5.py
+++ 2015/d5.py
@@ -3,20 +3,31 @@
 
 '''
 String examination:
+
+Part 1:
 * Nice strings:
     1) Contain at least 3 vowels (aeiou)
     2) Contains at least 1 letter that appears twice in a row (e.g., xx)
     3) Does NOT contain strings:  ab, cd, pq, or xy
 * Other strings are "naughty"
+
+Part 2:
+* Nice strings:
+    1) It contains a pair of any two letters that appears at least twice in the
+       string without overlapping, like xyxy (xy) or aabcdefgaa (aa), but not
+       like aaa (aa, but it overlaps).
+    2) It contains at least one letter which repeats with exactly one letter
+       between them, like xyx, abcdefeghi (efe), or even aaa.
 '''
 
 
 # INFILE = 'd5t1.txt'
-INFILE = 'd5.txt'
+INFILE = 'd5t2.txt'
+# INFILE = 'd5.txt'
 
 
-from collections import Counter
-from itertools import pairwise
+from collections import Counter, defaultdict
+from itertools import islice, pairwise
 
 
 def nice_string(string, verbose=False):
@@ -57,12 +68,47 @@ def nice_string(string, verbose=False):
     return True
 
 
+def nice_string2(string, verbose=True):
+    '''
+    1) It contains a pair of any two letters that appears at least twice in the
+       string without overlapping, like xyxy (xy) or aabcdefgaa (aa), but not
+       like aaa (aa, but it overlaps).
+    2) It contains at least one letter which repeats with exactly one letter
+       between them, like xyx, abcdefeghi (efe), or even aaa.
+    '''
+    count = defaultdict(int)
+    for letter1, letter2 in zip(islice(string, 0, None, 2), islice(string, 1, None, 2)):
+        count[(letter1, letter2)] += 1
+
+    # Check for item 1:
+    if all(item < 2 for item in count.values()):
+        if verbose:
+            print(f'String "{string}" failed test 1 - naughty.')
+        return False
+
+    # Check for item 2:
+    if all(
+        pair1[0] != pair2[1]
+        for pair1, pair2 in zip(
+            pairwise(string), pairwise(islice(string, 1, None))
+        )
+    ):
+        if verbose:
+            print(f'String "{string}" failed test 2 - naughty.')
+        return False
+
+    return True
+
+
 def main():
     string_count = dict(total=0, nice=0, naughty=0)
     with open(INFILE) as infile:
         for line in infile:
             string_count['total'] += 1
-            if nice_string(line.strip()):
+            # Part 1:
+            # if nice_string(line.strip()):
+            # Part 2:
+            if nice_string2(line.strip()):
                 string_count['nice'] += 1
             else:
                 string_count['naughty'] += 1
