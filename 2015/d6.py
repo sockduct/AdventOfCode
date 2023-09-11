45814ed0f77b1ac5057fdc3b409cb1e9e869424a
James Small <james.r.small@outlook.com>
2023-09-11 10:07:43 -0400

    Update:  Complete day 6, part 1 and part 2 code challenges

--- 2015/d6.py
+++ 2015/d6.py
@@ -14,20 +14,33 @@
 
 
 # INFILE = 'd6t1.txt'
-INFILE = 'd6t2.txt'
-# INFILE = 'd6.txt'
-# SIZE = 1_000
-SIZE = 10
+# INFILE = 'd6t2.txt'
+# INFILE = 'd6t3.txt'
+# INFILE = 'd6t4.txt'
+INFILE = 'd6.txt'
+SIZE = 1_000
+# SIZE = 20
 
 
+from collections import namedtuple
 from pathlib import Path
 
 
 class Grid:
-    def __init__(self, x, y):
+    def __init__(self, x, y, val=bool):
         self.x = x
         self.y = y
-        self.grid = [[False for _ in range(x)] for _ in range(y)]
+        if val.__name__ == 'bool':
+            self.simple = True
+            default_init = (True, False, lambda x: not x)
+        elif val.__name__ == 'int':
+            self.simple = False
+            default_init = (1, 0, 2)
+        else:
+            raise ValueError(f'Expected val of bool|int, got "{val.__name__}"')
+
+        self.vals = namedtuple('Values', 'on off toggle', defaults=default_init)()
+        self.grid = [[self.vals.off for _ in range(x)] for _ in range(y)]
 
     def __repr__(self):
         return f'Grid({self.x}, {self.y})'
@@ -50,9 +63,9 @@ class Grid:
             y = self.y
 
         out = ''
-        for y in range(y):
-            for x in range(x):
-                out += f'{int(self.grid[y][x])}'
+        for row in range(y):
+            for col in range(x):
+                out += f'{int(self.grid[row][col])}'
             if xover:
                 out += '...'
             out += '\n'
@@ -71,19 +84,28 @@ class Grid:
     def update(self, p1, p2, op):
         match op:
             case 'on':
-                alt = False
-                val = True
+                if self.simple:
+                    alt = False
+                    val = self.vals.on
+                else:
+                    alt = True
+                    val = lambda x: x + self.vals.on
             case 'off':
-                alt = False
-                val = False
+                if self.simple:
+                    alt = False
+                    val = self.vals.off
+                else:
+                    alt = True
+                    val = lambda x: x - 1 if x > 1 else 0
             case 'toggle':
                 alt = True
+                val = self.vals.toggle if self.simple else (lambda x: x + 2)
             case _:
                 raise ValueError(f'Expected on|off|toggle, got "{op}"')
 
         for y in range(p1[1], p2[1] + 1):
             for x in range (p1[0], p2[0] + 1):
-                self.grid[y][x] = not self.grid[y][x] if alt else val
+                self.grid[y][x] = val(self.grid[y][x]) if alt else val
 
     def on(self, p1, p2):
         self.update(p1, p2, 'on')
@@ -102,30 +124,42 @@ def get_coords(p1, p2):
     return p1, p2
 
 
-def process(line, grid):
+def process(line, grid, verbose=False):
     match line.split():
         case ['turn', 'on', p1, 'through', p2]:
+            if verbose:
+                print(f'Turning on {p1} - {p2}')
             p1, p2 = get_coords(p1, p2)
             grid.on(p1, p2)
         case ['toggle', p1, 'through', p2]:
+            if verbose:
+                print(f'Toggling {p1} - {p2}')
             p1, p2 = get_coords(p1, p2)
             grid.toggle(p1, p2)
         case ['turn', 'off', p1, 'through', p2]:
+            if verbose:
+                print(f'Turning off {p1} - {p2}')
             p1, p2 = get_coords(p1, p2)
             grid.off(p1, p2)
         case _:
             raise ValueError(f'Expected turn on|toggle|turn off..., got "{line.split()}"')
 
 
-def main(verbose=True):
-    grid = Grid(SIZE, SIZE)
+def main(verbose=False):
+    # Override default for part 2
+    # grid = Grid(SIZE, SIZE)
+    grid = Grid(SIZE, SIZE, int)
+    if verbose:
+        print(f'Initial grid:\n{grid}')
     dirloc = Path(__file__).parent
     with open(dirloc/INFILE) as infile:
         for line in infile:
-            process(line.strip(), grid)
+            process(line.strip(), grid, verbose)
+            if verbose:
+                print(f'Grid now:\n{grid}')
 
     if verbose:
-        print(f'Light grid:\n{grid}')
+        print(f'Final light grid:\n{grid}')
     print(f'Light grid has {grid.status()[0]:,} lights lit.')
 
 
