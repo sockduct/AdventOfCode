c7177eb257dce8817a2c2a80566757333215fa4b
James Small <james.r.small@outlook.com>
2023-09-15 10:43:37 -0400

    Update:  Continue working on day 7, part 1 code challenge

--- 2015/d7.py
+++ 2015/d7.py
@@ -31,7 +31,9 @@ def isnum(wire):
 
 def gettype(val, wires):
     # Can we get a number?
-    if (res := isnum(val)) or (res := isnum(wires[val])):
+    # Edge case - 0 is a number but not "truthy", add check for is not None:
+    if ((res := isnum(val)) or res is not None or
+            (res := isnum(wires[val])) or res is not None):
         return res
     # Is there an expression present?
     elif wires[val]:
@@ -60,26 +62,46 @@ def parse(line, wires):
     '''
     match line.split():
         case [inval, '->', outline]:
+            if wires.get(outline):
+                raise ValueError(f'wire {outline} already has value {wires[outline]}')
             wires[outline] = gettype(inval, wires)
         case ['NOT', inval, '->', outline]:
             val = gettype(inval, wires)
+            if wires.get(outline):
+                raise ValueError(f'wire {outline} already has value {wires[outline]}')
             wires[outline] = (
                 MAXVAL + ~val if isinstance(val, int) else ['NOT', val]
             )
         case [lval, 'AND', rval, '->', outline]:
             status, val1, val2 = binop(lval, rval, wires)
+            if wires.get(outline):
+                raise ValueError(f'wire {outline} already has value {wires[outline]}')
             wires[outline] = val1 & val2 if status else [val1, 'AND', val2]
         case [lval, 'OR', rval, '->', outline]:
             status, val1, val2 = binop(lval, rval, wires)
+            if wires.get(outline):
+                raise ValueError(f'wire {outline} already has value {wires[outline]}')
             wires[outline] = val1 | val2 if status else [val1, 'OR', val2]
         case [lval, 'LSHIFT', rval, '->', outline]:
             status, val1, val2 = binop(lval, rval, wires)
+            if wires.get(outline):
+                raise ValueError(f'wire {outline} already has value {wires[outline]}')
             wires[outline] = val1 << val2 if status else [val1, 'LSHIFT', val2]
         case [lval, 'RSHIFT', rval, '->', outline]:
             status, val1, val2 = binop(lval, rval, wires)
+            if wires.get(outline):
+                raise ValueError(f'wire {outline} already has value {wires[outline]}')
             wires[outline] = val1 >> val2 if status else [val1, 'RSHIFT', val2]
         case _:
             raise ValueError(f'Unexpected sequence:  {line}')
+
+
+def getwireval(wires, wireval):
+    '''
+    Recursively parse wires dict to calculate end value for wire passed in
+    wireval
+    '''
+    ...
 
 
 def main():
@@ -91,7 +113,10 @@ def main():
         for line in infile:
             parse(line.strip(), wires)
 
-    pprint(wires)
+    wireval = 'a'
+    res = getwireval(wires, wireval)
+    print(f'Value for wire {wireval}:  {res}')
+    # pprint(wires)
 
 
 if __name__ == '__main__':
