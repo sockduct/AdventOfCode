7217d189acf617b7d313fe49af4750afc39694fb
James Small <james.r.small@outlook.com>
2023-09-19 11:56:54 -0400

    Update:  Continue working on day 8, part 1 code challenge

--- 2015/d8.py
+++ 2015/d8.py
@@ -20,11 +20,20 @@ The only escape sequences used are:
 
 INFILE = 'd8.txt'
 # INFILE = 'd8t1.txt'
+#
+# "ASCII" values for characters:
+LOWER_A = 97
+LOWER_Z = 122
+BACKSLASH = 92
+QUOTE = 34
+LOWER_X = 120
+LOWERCASE = range(LOWER_A, LOWER_Z + 1)
 
 
 # Libraries:
 from dataclasses import dataclass
 from pathlib import Path
+import re
 from string import ascii_lowercase
 
 
@@ -33,33 +42,42 @@ from string import ascii_lowercase
 class Charcnt:
     code: int = 0
     mem: int = 0
+    remem: int = 0
+
+
+def recount(line, counter):
+    re1 = r'\\'
+    re2 = '"'
+    # Does't work - don't know how to make hex escape codes wildcards:
+    re3 = r'\\x[0-9a-f]{2}'
+    res = line
+    for pattern in (re1, re2, re3):
+        res = re.sub(pattern, '', res)
+
+    counter.remem += len(res)
 
 
 def process(line, counter):
     counter.code += len(line)
-    string = line.strip('"')
-    entity = None
+    # Strip quotes:
+    # string = line.strip('"')
+    string = line[1:-1]
+    entity = []
     char_count = 0
-    hex_esc = False
     for char in string:
-        if char in ascii_lowercase:
+        # if char in ascii_lowercase:
+        if not entity and char in LOWERCASE or char == QUOTE:
             char_count += 1
-        elif char == '"':
-            char_count += 1
-        elif hex_esc:
-            if len(entity) == 4:
-                char_count += 1
-                entity = None
-                hex_esc = False
         elif entity:
-            entity += char
-            if entity == '\\\\':
+            entity.append(char)
+            if entity == [BACKSLASH, BACKSLASH]:
                 char_count += 1
-                entity = None
-            elif entity == '\\x':
-                hex_esc = True
+                entity.clear()
+            elif entity[:2] == [BACKSLASH, LOWER_X] and len(entity) == 4:
+                char_count += 1
+                entity.clear()
         else:
-            entity = char
+            entity.append(char)
 
     counter.mem += char_count
 
@@ -67,7 +85,7 @@ def process(line, counter):
 def main():
     parent_dir = Path(__file__).parent
     counter = Charcnt()
-    with open(parent_dir/INFILE) as infile:
+    with open(parent_dir/INFILE, mode='rb') as infile:
         for line in infile:
             process(line.strip(), counter)
 
