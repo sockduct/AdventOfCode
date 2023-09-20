3e56e0fb9d28177c737a515f194524aa4cc005be
James Small <james.r.small@outlook.com>
2023-09-20 15:21:20 -0400

    Update:  Complete day 8, part 1 code challenge

--- 2015/d8.py
+++ 2015/d8.py
@@ -42,14 +42,22 @@ from string import ascii_lowercase
 class Charcnt:
     code: int = 0
     mem: int = 0
-    remem: int = 0
+    bs: int = 0
+    dq: int = 0
+    he: int = 0
 
 
 def recount(line, counter):
-    re1 = r'\\'
-    re2 = '"'
-    # Does't work - don't know how to make hex escape codes wildcards:
-    re3 = r'\\x[0-9a-f]{2}'
+    re1 = br'\\\\'
+    re2 = b'"'
+    # Does't work in Python
+    # * '\\' + 'x' doesn't match <backslash> + x
+    # * <backslash> + xHH is always interpreted as single character, can't split
+    #   it up
+    # * e.g., doesn't work:  re3 = r'\\x[0-9a-f]{2}'
+    #
+    # Note:  This only works when read file in as binary file
+    re3 = b'\\' + b'x'
     res = line
     for pattern in (re1, re2, re3):
         res = re.sub(pattern, '', res)
@@ -58,28 +66,46 @@ def recount(line, counter):
 
 
 def process(line, counter):
+    '''
+    PowerShell
+    * Count occurrences of '\\' (d8.txt = 111):
+        (sls '\\\\' -allmatches d8.txt).matches.count
+    * Count occurrences of '\"' (d8.txt = 275 w/o striping edges quotes,
+      otherwise 270):
+        (sls '\\"' -allmatches d8.txt).matches.count
+    * Count occurrences of <backslash> + 'xHH' (d8.txt = 123):
+        (sls '\\' + 'x[0-9a-f]{2}' -allmatches d8.txt).matches.count
+        Note:  Have to separate backslashes and x or Python tries to interpret
+               as hex-escape!
+    '''
     counter.code += len(line)
     # Strip quotes:
     # string = line.strip('"')
     string = line[1:-1]
     entity = []
-    char_count = 0
-    for char in string:
+    for index, char in enumerate(string):
+        # For debugging only:
+        curchar = chr(char)
+        curindex = index
         # if char in ascii_lowercase:
-        if not entity and char in LOWERCASE or char == QUOTE:
-            char_count += 1
+        if not entity and char in LOWERCASE:
+            counter.mem += 1
         elif entity:
             entity.append(char)
             if entity == [BACKSLASH, BACKSLASH]:
-                char_count += 1
+                counter.mem += 1
+                counter.bs += 1
+                entity.clear()
+            elif entity == [BACKSLASH, QUOTE]:
+                counter.mem += 1
+                counter.dq += 1
                 entity.clear()
             elif entity[:2] == [BACKSLASH, LOWER_X] and len(entity) == 4:
-                char_count += 1
+                counter.mem += 1
+                counter.he += 1
                 entity.clear()
         else:
             entity.append(char)
-
-    counter.mem += char_count
 
 
 def main():
@@ -90,7 +116,8 @@ def main():
             process(line.strip(), counter)
 
     print(f'In-code = {counter.code}\nIn-memory =  {counter.mem}\n'
-          f'Difference = {counter.code - counter.mem}')
+          f'Backslashes = {counter.bs}\nDouble-quotes = {counter.dq}\n'
+          f'Hex-escapes = {counter.he}\nDifference = {counter.code - counter.mem}')
 
 
 if __name__ == '__main__':
