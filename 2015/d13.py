762893d337117d70d5d744f041ee0421bd55ee2a
James Small <james.r.small@outlook.com>
2023-10-06 20:18:25 -0400

    Add:  Complete day 13, part 1 code challenge

---
+++ 2015/d13.py
@@ -0,0 +1,68 @@
+#! /usr/bin/env python3
+
+
+'''
+Optimal Seating Arrangement:
+* Read in list of people - for each person states happiness differential if
+  next to another person (positive or negative)
+* Each person next to two people (treat list as circular)
+* Find arrangement of people that maximizes happiness
+'''
+
+
+INFILE = 'd13.txt'
+# INFILE = 'd13t1.txt'
+
+
+# Libraries:
+from collections import defaultdict
+from itertools import permutations
+from pathlib import Path
+from pprint import pprint
+
+
+def parse(line, people):
+    line = line.strip('\n.')
+    person, _, direction, number, *_, peer = line.split()
+    people[person][peer] = int(number) if direction == 'gain' else -int(number)
+
+
+def happiness(arrangement, people):
+    score = 0
+
+    for index, person in enumerate(arrangement):
+        left = index - 1 if index != 0 else -1
+        right = index + 1 if index < len(arrangement) - 1 else 0
+        score += people[person][arrangement[left]] + people[person][arrangement[right]]
+
+    return score
+
+
+def max_permute(people, verbose=False):
+    max_score = 0
+
+    arrangements = permutations(people)
+    for arrangement in arrangements:
+        score = happiness(arrangement, people)
+        if verbose:
+            print(f'Arrangement:  {arrangement} => Happiness score of {score}')
+        if score > max_score:
+            max_score = score
+
+    return max_score
+
+
+def main():
+    people = defaultdict(dict)
+    with open(Path(__file__).parent/INFILE) as infile:
+        for line in infile:
+            parse(line, people)
+
+    # pprint(people)
+
+    res = max_permute(people)
+    print(f'Happiness change for optimal seating:  {res:,}')
+
+
+if __name__ == '__main__':
+    main()
