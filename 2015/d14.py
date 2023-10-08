1ce9fa6e350fccd9dd49a83f40b53bb3ca2bb28d
James Small <james.r.small@outlook.com>
2023-10-08 14:14:18 -0400

    Update:  Complete day 14, part 2 code challenge

--- 2015/d14.py
+++ 2015/d14.py
@@ -16,33 +16,77 @@ INFILE = 'd14.txt'
 
 
 # Libraries:
+from dataclasses import dataclass
 from pathlib import Path
 from pprint import pprint
 
 
+# Types:
+@dataclass
+class Reindeer:
+    speed: int
+    on: int
+    rest: int
+    dist: int = 0
+    score: int = 0
+
+
 def parse(line, reindeer):
     line = line.strip('\n.')
-    name, _, _, speed, _, _, workint, *_, restint, _ = line.split()
-    reindeer[name] = dict(speed=int(speed), workint=int(workint), restint=int(restint))
+    name, _, _, speed, _, _, on, *_, rest, _ = line.split()
+    reindeer[name] = Reindeer(speed=int(speed), on=int(on), rest=int(rest))
 
 
 def max_dist(reindeer, dist):
     max_dist = 0
 
     for deer in reindeer:
-        combined = reindeer[deer]['workint'] + reindeer[deer]['restint']
+        combined = reindeer[deer].on + reindeer[deer].rest
         intervals, remainder = divmod(dist, combined)
-        deer_dist = intervals * reindeer[deer]['speed'] * reindeer[deer]['workint']
+        deer_dist = intervals * reindeer[deer].speed * reindeer[deer].on
 
-        if 0 < remainder < reindeer[deer]['workint']:
-            deer_dist += reindeer[deer]['speed'] * remainder
-        elif remainder >= reindeer[deer]['workint']:
-            deer_dist += reindeer[deer]['speed'] * reindeer[deer]['workint']
+        if 0 < remainder < reindeer[deer].on:
+            deer_dist += reindeer[deer].speed * remainder
+        elif remainder >= reindeer[deer].on:
+            deer_dist += reindeer[deer].speed * reindeer[deer].on
 
         if deer_dist > max_dist:
             max_dist = deer_dist
 
     return max_dist
+
+
+def score2(reindeer, dist):
+    '''
+    Revised scoring system:
+    * 1 point to reindeer in the lead after each second
+        * If multiple reindeer tied for 1st, they each get 1 point
+    * How many points does winning reindeer have?
+    '''
+    max_dist = 0
+    cur_dist = {}
+
+    for time in range(1, dist + 1):
+        # Calculate current distance for each reindeer:
+        for deer in reindeer:
+            combined = reindeer[deer].on + reindeer[deer].rest
+            if 0 < time % combined <= reindeer[deer].on:
+                reindeer[deer].dist += reindeer[deer].speed
+
+            if reindeer[deer].dist > max_dist:
+                max_dist = reindeer[deer].dist
+
+        # Add point to leader(s):
+        for deer in reindeer:
+            if reindeer[deer].dist == max_dist:
+                reindeer[deer].score += 1
+
+    max_points = 0
+    for deer in reindeer:
+        if reindeer[deer].score > max_points:
+            max_points = reindeer[deer].score
+
+    return max_points
 
 
 def main():
@@ -57,8 +101,10 @@ def main():
     # dist = 1_000
     dist = 2_503
     res = max_dist(reindeer, dist)
+    res2 = score2(reindeer, dist)
 
     print(f'Farthest reindeer went {res:,} km.')
+    print(f'Reindeer with most points has {res2:,} points.')
 
 
 if __name__ == '__main__':
