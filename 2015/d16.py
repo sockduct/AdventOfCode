77d6768be90833c4248c9b114f12881d721db157
James Small <james.r.small@outlook.com>
2023-10-11 07:50:29 -0400

    Add:  Begin working on day 16, part 1 code challenge

---
+++ 2015/d16.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python3
+
+
+'''
+Which Aunt Sue?
+'''
+
+
+# INFILE = 'd16.txt'
+INFILE = 'd16t1.txt'
+
+
+# Libraries:
+
+
+def main():
+    ...
+
+
+if __name__ == '__main__':
+    main()
