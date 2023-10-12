1f1245ce9fd1d48d1417d3481e5a3696c3f006e0
James Small <james.r.small@outlook.com>
2023-10-12 08:34:15 -0400

    Update:  Continue working on day 16, part 1 code challenge

--- 2015/d16.py
+++ 2015/d16.py
@@ -6,15 +6,45 @@ Which Aunt Sue?
 '''
 
 
-# INFILE = 'd16.txt'
-INFILE = 'd16t1.txt'
+INFILE = 'd16.txt'
+INFILE2 = 'd16clues.txt'
 
 
 # Libraries:
+from dataclasses import dataclass
+import re
+
+
+# Types:
+@dataclass
+class Clues:
+    children: int | None = None
+    cats: int | None = None
+    samoyeds: int | None = None
+    pomeranians: int | None = None
+    akitas: int | None = None
+    vizslas: int | None = None
+    goldfish: int | None = None
+    trees: int | None = None
+    cars: int | None = None
+    perfumes: int | None = None
 
 
 def main():
-    ...
+    clues = Clues()
+    with open(INFILE2) as infile:
+        for line in infile:
+            key, value = line.split()
+            key = key.strip(':')
+            clues.key = int(value)
+
+    sues = {}
+    with open(INFILE) as infile:
+        for line in infile:
+            sue_number, items = re.match(r'Sue (\d+): (.*)', line).groups()
+            items = items.split(',')
+            items = [item.split() for item in items]
+            sues[sue_number] = { }
 
 
 if __name__ == '__main__':
