26b8bb7e1891af33152cf224512091c9ee95bc6f
James Small <james.r.small@outlook.com>
2023-10-23 18:05:10 -0400

    Update:  Complete day 20, part 2 code challenge

--- 2015/d20.py
+++ 2015/d20.py
@@ -22,6 +22,34 @@ def calc(n: int) -> int:
     return sum(multiple * value for value in range(1, n + 1) if n % value == 0)
 
 
+def calc2(n: int) -> int:
+    '''
+    The Elves decide they don't want to visit an infinite number of houses.
+    Instead, each Elf will stop after delivering presents to 50 houses. To make
+    up for it, they decide to deliver presents equal to eleven times their
+    number at each house.
+
+    sum(
+        multiple X each value in the range 1 to n + 1
+        if n divides into value (with no remainder) and if value * limiter < n
+    )
+    '''
+    multiple = 11
+    limiter = 50
+
+    '''
+    total = 0
+    for value in range(1, n + 1):
+        if n % value == 0 and value * limiter >= n:
+            total += multiple * value
+
+    return total
+    '''
+
+    return sum(multiple * value for value in range(1, n + 1)
+               if n % value == 0 and value * limiter >= n)
+
+
 def main(verbose: bool=False) -> None:
     cwd = Path(__file__).parent
 
@@ -31,11 +59,17 @@ def main(verbose: bool=False) -> None:
 
     print(f'Target:  {target:,}')
 
-    for i in range(770_000, 2_000_000):
+    # for i in range(1, 11):
+    # for i in range(770_000, 2_000_000):  # For Part 1
+    for i in range(785_000, 2_000_000):  # For Part 2
+        # Between 776,160 - 803,880
+        res = calc2(i)
         if verbose or i % 1_000 == 0:
-            print(f'House {i:,} got {calc(i):,} presents.')
-        if res := calc(i) >= target:
-            print(f'House {i:,} got {calc(i):,} presents.')
+            # print(f'House {i:,} got {res:,} presents.')
+            print(f'House {i:,} got {res:,} presents.')
+        if res >= target:
+            # print(f'House {i:,} got {res:,} presents.')
+            print(f'House {i:,} got {res:,} presents.')
             break
 
 
