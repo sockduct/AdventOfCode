31b062e3b10ab3de734d06518ab1257b13466c6a
James Small <james.r.small@outlook.com>
2023-12-29 12:51:09 -0500

    Update:  Complete day 5, part 2 code challenge

--- 2016/d5.py
+++ 2016/d5.py
@@ -22,22 +22,39 @@ from pathlib import Path
 
 
 # Module
-def get_pass(data: str, verbose: bool=True) -> str:
+def get_pass(data: str, use_pos: bool=False, verbose: bool=True) -> str:
+    valid_pos = set(range(8))
     password = []
+    if use_pos:
+        pos_pass = ['' for _ in range(8)]
+        pos_found = [False for _ in range(8)]
+
     for i in count():
         candidate = bytes(data + str(i), encoding='utf8')
         res = md5(candidate).hexdigest()
         if res.startswith('00000'):
-            password.append(res[5])
-            if len(password) >= 8:
+            if use_pos:
+                pos = int(res[5], 16)
+                if pos in valid_pos and not pos_found[pos]:
+                    pos_pass[pos] = res[6]
+                    pos_found[pos] = True
+                if verbose:
+                    print(f'Found position {pos:>2}:  {res[6]} - {sum(pos_found)} of 8')
+            else:
+                if verbose:
+                    print(f'Found next character:  {res[5]}')
+                password.append(res[5])
+            if (not use_pos and len(password) >= 8) or (use_pos and all(pos_pass)):
                 break
+        '''
         if verbose and i % 1_000_000 == 0:
             print('.', end='', flush=True)
+        '''
 
     if verbose:
         print()
 
-    return ''.join(password)
+    return ''.join(pos_pass) if use_pos else ''.join(password)
 
 
 def main() -> None:
@@ -45,7 +62,11 @@ def main() -> None:
     with open(cwd/INFILE) as infile:
         data = infile.read().strip()
 
-    password = get_pass(data)
+    # Part 1:
+    # password = get_pass(data)
+    #
+    # Part 2:
+    password = get_pass(data, use_pos=True)
     print(f'Password:  {password}')
 
 
