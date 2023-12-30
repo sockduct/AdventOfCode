88342175c47fe3269787955c8190e03306711fca
James Small <james.r.small@outlook.com>
2023-12-30 15:19:32 -0500

    Update:  Complete day 6, part 2 code challenge

--- 2016/d6.py
+++ 2016/d6.py
@@ -17,24 +17,30 @@ from pathlib import Path
 
 
 # Module
-def get_mcc_col(data: list[str]) -> str:
+def get_mcc_col(data: list[str], least: bool=False) -> str:
     word = []
     for col in range(len(data[0])):
         col_data = [row[col] for row in data]
         # Counter.most_common returns a list of tuples
         # Extract the character (first tuple element) from the first tuple:
-        word.append(Counter(col_data).most_common(1)[0][0])
+        letter_freq = Counter(col_data).most_common()
+        letter = letter_freq[-1][0] if least else letter_freq[0][0]
+        word.append(letter)
 
     return ''.join(word)
 
 
 def main() -> None:
-    data = []
+    data: list[str] = []
     cwd = Path(__file__).parent
     with open(cwd/INFILE) as infile:
         data.extend(line.strip() for line in infile)
 
-    word = get_mcc_col(data)
+    # Part 1:
+    # word = get_mcc_col(data)
+    #
+    # Part 2:
+    word = get_mcc_col(data, least=True)
     print(f'Error-corrected message:  {word}')
 
 
