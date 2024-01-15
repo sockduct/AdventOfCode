83f4f0e1b86c9c3f7112c6c6abe6eafe8e9c90f9
James Small <james.r.small@outlook.com>
2024-01-15 11:28:25 -0500

    Update:  Complete day 9, part 1.  Begin working on part 2.

--- 2016/d9.py
+++ 2016/d9.py
@@ -14,7 +14,8 @@ Decompress string
 
 
 # INFILE = 'd9.txt'
-INFILE = 'd9t1.txt'
+# INFILE = 'd9t1.txt'
+INFILE = 'd9t2.txt'
 
 
 # Libraries:
@@ -23,17 +24,28 @@ from reprlib import repr as altrepr
 
 
 # Module:
-def parse(line: str) -> tuple[str, int]:
+def parse(line: str, *, recursive: bool=False) -> tuple[str, int]:
     output = ''
     index = 0
     while index < len(line):
         start = line.find('(', index)
         if start >= 0:
             end = line.find(')', start)
-            output += line[index:start]
+            if not recursive:
+                output += line[index:start]
+            else:
+                output = line[:start]
+
             count, repeat = [int(n) for n in line[start + 1:end].split('x')]
             output += line[end + 1:end + 1 + count] * repeat
-            index += end + 1 + count
+            index = start
+            if not recursive:
+                index = end + 1 + count
+            else:
+                line = output + line[end + 1 + count:]
+                index = line.find('(')
+                if index == -1:
+                    index = len(output)
         else:
             output += line[index:]
             break
@@ -45,7 +57,13 @@ def main() -> None:
     cwd = Path(__file__).parent
     with open(cwd/INFILE) as infile:
         for line in infile:
-            res = parse(line.strip())
+            # Part 1:
+            # res = parse(line.strip())
+            #
+            # Part 2:
+            ### Too slow - need alternate approach.  Maybe just find markers and
+            ### calculate number of characters?
+            res = parse(line.strip(), recursive=True)
             print(f'Decompressed "{altrepr(line.strip())}" to "{altrepr(res[0])}", length is {res[1]:,}')
 
 
